f0eb4db94886087bc96a1bc2e3b1d2515ee21f9f
Dmitrii Shelonin <cheisesu@gmail.com>
2026-03-04 19:35:31 +0100

    [1] added more tests on cancel

--- Tests/NetworkLibTests/Tests/RawSocketTests/RawSocketTests+UDP+Cancel.swift
+++ Tests/NetworkLibTests/Tests/RawSocketTests/RawSocketTests+UDP+Cancel.swift
@@ -16,7 +16,6 @@ class RawSocketTests_UDP_Cancel: XCTestCase {
 
         let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
                                    transport: transport, timeout: timeout, sni: "localhost")
-        defer { socket.cancel() }
 
         let cancelExpect = expectation(description: "Callback called")
         socket.cancel {
@@ -34,7 +33,6 @@ class RawSocketTests_UDP_Cancel: XCTestCase {
 
         let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
                                    transport: transport, timeout: timeout, sni: "localhost")
-        defer { socket.cancel() }
 
         let cancelExpect = expectation(description: "Callback called")
         socket.connect { _, _ in
@@ -75,7 +73,6 @@ class RawSocketTests_UDP_Cancel: XCTestCase {
 
         let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
                                    transport: transport, timeout: timeout, sni: "localhost")
-        defer { socket.cancel() }
 
         let cancelExpect = expectation(description: "Callback called")
         socket.cancel {
@@ -133,6 +130,7 @@ class RawSocketTests_UDP_Cancel: XCTestCase {
 
         let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
                                    transport: transport, timeout: timeout, sni: "localhost")
+        defer { socket.cancel() }
 
         let cancelExpect = expectation(description: "Callback called")
         socket.connect { _, error in
@@ -212,6 +210,7 @@ class RawSocketTests_UDP_Cancel: XCTestCase {
 
         let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
                                    transport: transport, timeout: timeout, sni: "localhost")
+        defer { socket.cancel() }
 
         let cancelExpect = expectation(description: "Callback called")
         socket.connect { _, error in
@@ -245,26 +244,167 @@ class RawSocketTests_UDP_Cancel: XCTestCase {
     }
 
     func test_Cancel_WhenNotConnected_MultipleCallbacks_CallbacksCalled() async throws {
-        throw XCTSkip("Not implemented")
+        let transport: NetTransport = .udp
+        let timeout: TimeInterval = 0
+        let server = try ServerMock(transport: transport, isSecure: true)
+        defer { server.stop() }
+        let port = try await server.start()
+
+        let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
+                                   transport: transport, timeout: timeout, sni: "localhost")
+
+        let cancelExpect = expectation(description: "Callback called")
+        cancelExpect.expectedFulfillmentCount = 2
+        socket.cancel {
+            cancelExpect.fulfill()
+        }
+        socket.cancel {
+            cancelExpect.fulfill()
+        }
+        await fulfillment(of: [cancelExpect], timeout: 1)
     }
 
     func test_Cancel_WhenConnecting_MultipleCallbacks_CallbacksCalled() async throws {
-        throw XCTSkip("Not implemented")
+        let transport: NetTransport = .udp
+        let timeout: TimeInterval = 0
+        let server = try ServerMock(transport: transport, isSecure: true)
+        defer { server.stop() }
+        let port = try await server.start()
+
+        let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
+                                   transport: transport, timeout: timeout, sni: "localhost")
+
+        let cancelExpect = expectation(description: "Callback called")
+        cancelExpect.expectedFulfillmentCount = 2
+        socket.connect { _, _ in
+        }
+        socket.cancel {
+            cancelExpect.fulfill()
+        }
+        socket.cancel {
+            cancelExpect.fulfill()
+        }
+        await fulfillment(of: [cancelExpect], timeout: 1)
     }
 
     func test_Cancel_WhenConnected_MultipleCallbacks_CallbacksCalled() async throws {
-        throw XCTSkip("Not implemented")
+        let transport: NetTransport = .udp
+        let timeout: TimeInterval = 0
+        let server = try ServerMock(transport: transport, isSecure: true)
+        defer { server.stop() }
+        let port = try await server.start()
+
+        let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
+                                   transport: transport, timeout: timeout, sni: "localhost")
+        defer { socket.cancel() }
+
+        let cancelExpect = expectation(description: "Callback called")
+        cancelExpect.expectedFulfillmentCount = 2
+        socket.connect { _, error in
+            XCTAssertNil(error)
+            socket.cancel {
+                cancelExpect.fulfill()
+            }
+            socket.cancel {
+                cancelExpect.fulfill()
+            }
+        }
+        await fulfillment(of: [cancelExpect], timeout: 3)
     }
 
     func test_Cancel_WhenCancelled_MultipleCallbacks_CallbacksCalled() async throws {
-        throw XCTSkip("Not implemented")
+        let transport: NetTransport = .udp
+        let timeout: TimeInterval = 0
+        let server = try ServerMock(transport: transport, isSecure: true)
+        defer { server.stop() }
+        let port = try await server.start()
+
+        let socket = try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
+                                   transport: transport, timeout: timeout, sni: "localhost")
+        defer { socket.cancel() }
+
+        let cancelExpect = expectation(description: "Callback called")
+        cancelExpect.expectedFulfillmentCount = 2
+        socket.cancel {
+            socket.cancel {
+                cancelExpect.fulfill()
+            }
+            socket.cancel {
+                cancelExpect.fulfill()
+            }
+        }
+        await fulfillment(of: [cancelExpect], timeout: 1)
+    }
+
+    func test_Cancel_WhenDeinited_CallbacksCalled() async throws {
+        let transport: NetTransport = .udp
+        let timeout: TimeInterval = 0
+        let server = try ServerMock(transport: transport, isSecure: true)
+        defer { server.stop() }
+        let port = try await server.start()
+
+        let box = _SocketBox()
+        box.set(try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
+                              transport: transport, timeout: timeout, sni: "localhost"))
+        defer { box.get()?.cancel() }
+
+        let cancelExpect = expectation(description: "Callback called")
+        box.get()?.cancel {
+            cancelExpect.fulfill()
+        }
+        box.set(nil)
+        await fulfillment(of: [cancelExpect], timeout: 1)
     }
 
     func test_Cancel_DeinitOnSameQueue_CallbacksCalled() async throws {
-        throw XCTSkip("Not implemented")
+        let transport: NetTransport = .udp
+        let timeout: TimeInterval = 0
+        let server = try ServerMock(transport: transport, isSecure: true)
+        defer { server.stop() }
+        let port = try await server.start()
+
+        let box = _SocketBox()
+        box.set(try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
+                              transport: transport, timeout: timeout, sni: "localhost"))
+        defer { box.get()?.cancel() }
+
+        let cancelExpect = expectation(description: "Callback called")
+        box.get()?.connect { _, error in
+            XCTAssertNil(error)
+            box.get()?.cancel {
+                cancelExpect.fulfill()
+            }
+            box.get()?.connect { _, _ in
+                box.set(nil)
+            }
+        }
+        await fulfillment(of: [cancelExpect], timeout: 3)
     }
 
     func test_Cancel_DeinitOnDifferentQueue_CallbacksCalled() async throws {
-        throw XCTSkip("Not implemented")
+        let transport: NetTransport = .udp
+        let timeout: TimeInterval = 0
+        let server = try ServerMock(transport: transport, isSecure: true)
+        defer { server.stop() }
+        let port = try await server.start()
+
+        let box = _SocketBox()
+        box.set(try RawSocket(endpoint: .hostPort(host: "127.0.0.1", port: port), maxDataBlock: 256,
+                              transport: transport, timeout: timeout, sni: "localhost"))
+        defer { box.get()?.cancel() }
+
+        let cancelExpect = expectation(description: "Callback called")
+        box.get()?.connect { _, error in
+            XCTAssertNil(error)
+            box.get()?.cancel {
+                cancelExpect.fulfill()
+            }
+            box.get()?.connect { _, _ in
+                DispatchQueue.global().async {
+                    box.set(nil)
+                }
+            }
+        }
+        await fulfillment(of: [cancelExpect], timeout: 3)
     }
 }
