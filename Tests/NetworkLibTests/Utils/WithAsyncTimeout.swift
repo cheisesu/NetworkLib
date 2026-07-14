0197df5b747eec34c17d26cd5dcb40452711f2c3
Dmitrii Shelonin <cheisesu@gmail.com>
2026-07-14 22:45:15 +0200

    [67] added with async timeout helper function

--- Tests/NetworkLibTests/Utils/WithAsyncTimeout.swift
+++ Tests/NetworkLibTests/Utils/WithAsyncTimeout.swift
@@ -2,10 +2,38 @@ import Foundation
 
 struct AsyncTimeoutError: Error {}
 
+@discardableResult
 func withAsyncTimeout<T: Sendable>(_ timeout: Duration, block: @escaping @Sendable () async throws -> T) async rethrows -> T {
     try await withThrowingTaskGroup { group in
         group.addTask {
             try await block()
+        }
+        group.addTask {
+            try await Task.sleep(until: .now + timeout)
+            throw AsyncTimeoutError()
+        }
+
+        defer { group.cancelAll() }
+        guard let result = try await group.next() else { throw AsyncTimeoutError() }
+        return result
+    }
+}
+
+@discardableResult
+func withAsyncTimeoutCancelationContinuation<T: Sendable>(
+    _ timeout: Duration,
+    block: @escaping @Sendable (_ continuation: CheckedContinuation<T, Error>) -> Void,
+    onCancel: @escaping @Sendable () -> Void
+) async throws -> T {
+    try await withThrowingTaskGroup { group in
+        group.addTask {
+            try await withTaskCancellationHandler {
+                try await withCheckedThrowingContinuation { continuation in
+                    block(continuation)
+                }
+            } onCancel: {
+                onCancel()
+            }
         }
         group.addTask {
             try await Task.sleep(until: .now + timeout)
