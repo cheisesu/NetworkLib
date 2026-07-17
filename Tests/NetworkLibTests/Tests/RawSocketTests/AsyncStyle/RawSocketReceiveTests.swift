6a87ce7d9430df6e63217467cfe7809036b5afcd
Dmitrii Shelonin <cheisesu@gmail.com>
2026-07-17 23:40:23 +0200

    [67] renamed test class

--- Tests/NetworkLibTests/Tests/RawSocketTests/AsyncStyle/RawSocketReceiveTests.swift
+++ Tests/NetworkLibTests/Tests/RawSocketTests/AsyncStyle/RawSocketReceiveTests.swift
@@ -3,11 +3,7 @@ import Testing
 import Network
 @testable import NetworkLib
 
-extension Tag.RawSocket {
-    @Tag static var receive: Tag
-}
-
-struct RawSocketReceiveTests {
+struct _RawSocketReceiveTests {
     @Test("When continuation is called multiple times it will fall with fatal error and test fail",
           .tags(.RawSocket.receive),
           arguments: [RawSocketTransport.tcp, .udp], [nil, "localhost"])
