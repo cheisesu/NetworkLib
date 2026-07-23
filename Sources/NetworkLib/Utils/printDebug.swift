e6f396ca39f8de28c119dedc5243b4a183a9284d
Dmitrii Shelonin <cheisesu@gmail.com>
2026-07-23 22:18:29 +0200

    [67] added time to printDebug

--- Sources/NetworkLib/Utils/printDebug.swift
+++ Sources/NetworkLib/Utils/printDebug.swift
@@ -2,13 +2,14 @@ import Foundation
 
 func printDebug(_ items: Any?..., separator: String = " ", terminator: String = "\n", defaultNil: String = "<??>") {
 #if DEBUG
-    let items = items.map {
+    let date = Date().formatted(.dateTime.hour().minute().second().secondFraction(.fractional(3)))
+    let items = [date] + items.map {
         if let item = $0 {
             String(describing: item)
         } else {
             defaultNil
         }
-    }.joined(separator: separator)
-    print(items, terminator: terminator)
+    }
+    print(items.joined(separator: separator), terminator: terminator)
 #endif
 }
