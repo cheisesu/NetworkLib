0186ed94a3b921e1584b1a29770decda13d518b2
Dmitrii Shelonin <cheisesu@gmail.com>
2026-08-03 22:35:11 +0200

    [67] added empty test suite

---
+++ Tests/NetworkLibTests/Tests/RawSocketTests/CallbackStyle/New/RawSocketTests+Send.swift
@@ -0,0 +1,23 @@
+import Testing
+import Foundation
+import Network
+@testable import NetworkLib
+
+extension Tag.RawSocket {
+    @Tag static var send: Tag
+}
+
+@Suite(.disabled("Not implemented"))
+struct RawSocketSendTests {
+    // MARK: SUCCESS IN DIFFERENT STATES
+
+    // MARK: ERRORS IN DIFFERENT STATES
+
+    // MARK: TIMEOUTS
+
+    // MARK: ERRORS BY SERVER BEHAVIOUR
+
+    // MARK: PROTOCOL ERRORS
+
+    // MARK: ENDPOINT ERRORS
+}
