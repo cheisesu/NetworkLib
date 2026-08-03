5f60c8e2fb330567bee9ba79ec1fc627d1dfe02b
Dmitrii Shelonin <cheisesu@gmail.com>
2026-08-03 23:25:16 +0200

    [67] added versions guard

--- Sources/NetworkLib/Utils/printDebug.swift
+++ Sources/NetworkLib/Utils/printDebug.swift
@@ -2,6 +2,7 @@ import Foundation
 
 func printDebug(_ items: Any?..., separator: String = " ", terminator: String = "\n", defaultNil: String = "<??>") {
 #if DEBUG
+    guard #available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *) else { return }
     let date = Date().formatted(.dateTime.hour().minute().second().secondFraction(.fractional(3)))
     let items = [date] + items.map {
         if let item = $0 {
