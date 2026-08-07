f5784229f95e6ecd3a2cee39ac1286586bea6688
Dmitrii Shelonin <cheisesu@gmail.com>
2026-08-08 00:43:34 +0200

    [67] updated tests for internal state checks

--- Tests/NetworkLibTests/Tests/RawSocketTests/CallbackStyle/New/RawSocketTests+InternalState.swift
+++ Tests/NetworkLibTests/Tests/RawSocketTests/CallbackStyle/New/RawSocketTests+InternalState.swift
@@ -7,62 +7,31 @@ extension Tag.RawSocket {
 }
 
 struct RawSocketInternalStateTests {
-    @Test(.tags(.RawSocket.internalState, .RawSocket.all))
-    func correctSequenceOfConnectThenCancel() async throws {
-        let server = try ServerMock(transport: .tcp, isSecure: false)
-        defer { server.stop() }
-        let port = try await server.start()
-        let config = RawSocketConfiguration("127.0.0.1", port, isSecure: false, sni: nil, transport: .tcp,
-                                            maxDataBlock: 256, timeout: 0)
-        let socket = try RawSocket(config)
-        defer { socket.cancel(nil) }
-
-        let (sequence, seqContinuation) = AsyncStream.makeStream(of: RawSocket._InternalState.self)
-        socket.onInternalStateChange = { oldState, newState in
-            seqContinuation.yield(newState)
-        }
-        let _: Void = try await withAsyncTimeoutCancelationContinuation(.seconds(2)) { continuation in
-            socket.connect { _ in
-                socket.cancel {
-                    seqContinuation.finish()
-                    continuation.resume()
-                }
-            }
-        } onCancel: {
-            socket.cancel(nil)
-            seqContinuation.finish()
-        }
-        let states = await sequence.reduce(into: []) { partialResult, state in
-            partialResult.append(state)
-        }
-        try #require(states == [.connecting, .connected, .cancelling, .closed])
-    }
-
-    @Test(.tags(.RawSocket.internalState, .RawSocket.all))
-    func oldStateIsLessThanNewState() async throws {
-        let server = try ServerMock(transport: .tcp, isSecure: false)
-        defer { server.stop() }
-        let port = try await server.start()
-        let config = RawSocketConfiguration("127.0.0.1", port, isSecure: false, sni: nil, transport: .tcp,
-                                            maxDataBlock: 256, timeout: 0)
-        let socket = try RawSocket(config)
+    @Test(.timeLimit(.minutes(1)), .tags(.RawSocket.internalState, .RawSocket.all),
+          arguments: [RawSocketTransport.tcp, .udp])
+    func connectThenCancel_CorrectSequence(_ transport: RawSocketTransport) async throws {
+        let timeout: TimeInterval = 0
+        let underlyingConnection = NWConnectionMock()
+        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: timeout,
+                                   maxDataBlock: 256, transport: transport)
         defer { socket.cancel(nil) }
 
         let (sequence, seqContinuation) = AsyncStream.makeStream(of: (RawSocket._InternalState, RawSocket._InternalState).self)
         socket.onInternalStateChange = { oldState, newState in
             seqContinuation.yield((oldState, newState))
         }
-        let _: Void = try await withAsyncTimeoutCancelationContinuation(.seconds(2)) { continuation in
+        let _: Void = try await withCheckedThrowingContinuation { continuation in
             socket.connect { _ in
                 socket.cancel {
                     seqContinuation.finish()
                     continuation.resume()
                 }
             }
-        } onCancel: {
-            socket.cancel(nil)
-            seqContinuation.finish()
         }
+
+        let pairs = await sequence.values
+        let states = pairs.map(\.1)
+        try #require(states == [.connecting, .connected, .cancelling, .closed])
         try await #require(sequence.allSatisfy { $0.0 < $0.1 })
     }
 }
