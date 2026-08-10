df9fe55314b1d30aaa92dbe0ec8c9ad519ff93f9
Dmitrii Shelonin <cheisesu@gmail.com>
2026-08-10 21:56:40 +0200

    [67] added test of keeping self reference

--- Tests/NetworkLibTests/Tests/RawSocketTests/CallbackStyle/New/RawSocketTests+InternalState.swift
+++ Tests/NetworkLibTests/Tests/RawSocketTests/CallbackStyle/New/RawSocketTests+InternalState.swift
@@ -10,9 +10,8 @@ struct RawSocketInternalStateTests {
     @Test(.timeLimit(.minutes(1)), .tags(.RawSocket.internalState, .RawSocket.all),
           arguments: [RawSocketTransport.tcp, .udp])
     func connectThenCancel_CorrectSequence(_ transport: RawSocketTransport) async throws {
-        let timeout: TimeInterval = 0
         let underlyingConnection = NWConnectionMock()
-        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: timeout,
+        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: 0,
                                    maxDataBlock: 256, transport: transport)
         defer { socket.cancel(nil) }
 
@@ -33,5 +32,37 @@ struct RawSocketInternalStateTests {
         let states = pairs.map(\.1)
         try #require(states == [.connecting, .connected, .cancelling, .closed])
         try await #require(sequence.allSatisfy { $0.0 < $0.1 })
+    }
+
+
+    @Test(.timeLimit(.minutes(1)), .tags(.RawSocket.internalState, .RawSocket.all))
+    func notDeinitsUntilClosed() async throws {
+        let underlyingConnection = NWConnectionMock()
+        var tempSocket: RawSocket? = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil,
+                                                   timeout: 0, maxDataBlock: 256, transport: .tcp)
+        let address = ReferencesCounter.shared.address(of: tempSocket)
+        let box = _SocketBox()
+        box.set(tempSocket)
+        tempSocket = nil
+        defer { box.get()?.cancel(nil) }
+        try #require(ReferencesCounter.shared.count(of: address) == 1)
+
+        let (sequence, seqContinuation) = AsyncStream.makeStream(of: (RawSocket._InternalState, RawSocket._InternalState).self)
+        box.get()?.onInternalStateChange = { oldState, newState in
+            seqContinuation.yield((oldState, newState))
+        }
+        let _: Void = try await withCheckedThrowingContinuation { continuation in
+            box.get()?.connect { _ in
+                box.get()?.cancel {
+                    seqContinuation.finish()
+                    continuation.resume()
+                }
+                box.set(nil)
+            }
+        }
+        let pairs = await sequence.values
+        let states = pairs.map(\.1)
+        try #require(states == [.connecting, .connected, .cancelling, .closed])
+        try #require(ReferencesCounter.shared.count(of: address) == 0)
     }
 }
