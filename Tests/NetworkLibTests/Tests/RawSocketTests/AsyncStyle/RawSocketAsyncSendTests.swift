54962dafcaa12b72111d432060ddcf23d1cccd87
Dmitrii Shelonin <cheisesu@gmail.com>
2026-08-21 22:16:40 +0200

    [67] updated async send tests

---
+++ Tests/NetworkLibTests/Tests/RawSocketTests/AsyncStyle/RawSocketAsyncSendTests.swift
@@ -0,0 +1,55 @@
+import Foundation
+import Testing
+import Network
+@testable import NetworkLib
+
+struct RawSocketAsyncSendTests {
+    @Test(.tags(.RawSocket.send, .RawSocket.all), .timeLimit(.minutes(1)), arguments: [RawSocketTransport.tcp, .udp])
+    func onSuccess(_ transport: RawSocketTransport) async throws {
+        let timeout: TimeInterval = 0
+        let data = Data("hello".utf8)
+        let underlyingConnection = NWConnectionMock()
+        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: timeout,
+                                   maxDataBlock: 256, transport: transport)
+        defer { socket.cancel(nil) }
+        try await socket.connect()
+        try await socket.send(data)
+        try #require(underlyingConnection.sendDataFull == data)
+    }
+
+    @Test(.tags(.RawSocket.send, .RawSocket.all), .timeLimit(.minutes(1)), arguments: [RawSocketTransport.tcp, .udp])
+    func onError(_ transport: RawSocketTransport) async throws {
+        let timeout: TimeInterval = 0
+        let expectedError = NWError.posix(.EINVAL)
+        let data = Data("hello".utf8)
+        let underlyingConnection = NWConnectionMock(overridedSendError: expectedError)
+        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: timeout,
+                                   maxDataBlock: 256, transport: transport)
+        defer { socket.cancel(nil) }
+        do {
+            try await socket.connect()
+            try await socket.send(data)
+            Issue.record("Unexpected entrance")
+        } catch let error where error == expectedError {
+        }
+    }
+
+    @Test(.tags(.RawSocket.send, .RawSocket.all), .timeLimit(.minutes(1)), arguments: [RawSocketTransport.tcp, .udp])
+    func whenTaskCancelled_ThrowsCancelledError(_ transport: RawSocketTransport) async throws {
+        let timeout: TimeInterval = 0
+        let data = Data("hello".utf8)
+        let underlyingConnection = NWConnectionMock(mode: [.methodSend, .dontCallCallback])
+        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: timeout,
+                                   maxDataBlock: 256, transport: transport)
+        defer { socket.cancel(nil) }
+        do {
+            try await socket.connect()
+            let task = Task {
+                try await socket.send(data)
+            }
+            task.cancel()
+            _ = try await task.value
+        } catch NWError.posix(.ECANCELED) {
+        }
+    }
+}
