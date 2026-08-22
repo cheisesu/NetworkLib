6be51a3ee22530fdcf50246feac0a94e41e738a2
Dmitrii Shelonin <cheisesu@gmail.com>
2026-08-22 08:52:34 +0200

    [67] updated async cancel tests

---
+++ Tests/NetworkLibTests/Tests/RawSocketTests/AsyncStyle/RawSocketAsyncCacncelTests.swift
@@ -0,0 +1,38 @@
+import Foundation
+import Testing
+import Network
+@testable import NetworkLib
+
+struct RawSocketAsyncCancelTests {
+    @Test(.tags(.RawSocket.connect, .RawSocket.all), .timeLimit(.minutes(1)), arguments: [RawSocketTransport.tcp, .udp])
+    func onSuccess(_ transport: RawSocketTransport) async throws {
+        let timeout: TimeInterval = 0
+        let underlyingConnection = NWConnectionMock()
+        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: timeout,
+                                   maxDataBlock: 256, transport: transport)
+        try await socket.connect()
+        await socket.cancel()
+        do {
+            try await socket.connect()
+            Issue.record("Unexpected entrance")
+        } catch NWError.posix(.ECANCELED) {
+        }
+    }
+
+    @Test(.tags(.RawSocket.connect, .RawSocket.all), .timeLimit(.minutes(1)), arguments: [RawSocketTransport.tcp, .udp])
+    func multipleCancels_AllComplete(_ transport: RawSocketTransport) async throws {
+        let timeout: TimeInterval = 0
+        let underlyingConnection = NWConnectionMock()
+        let socket = try RawSocket(underlyingConnection, accessQueue: nil, delegateQueue: nil, timeout: timeout,
+                                   maxDataBlock: 256, transport: transport)
+        try await socket.connect()
+        async let cancel1 = socket.cancel()
+        async let cancel2 = socket.cancel()
+        _ = await [cancel1, cancel2]
+        do {
+            try await socket.connect()
+            Issue.record("Unexpected entrance")
+        } catch NWError.posix(.ECANCELED) {
+        }
+    }
+}
